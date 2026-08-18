4d1e18aef83b979150778681b896512fc4bc4757
tig05015 <tig05015@naver.com>
2026-08-18 10:15:50 +0900

    9229 수정

--- week05/swea_9229_한빈이와SpotMart/Jimin.java
+++ week05/swea_9229_한빈이와SpotMart/Jimin.java
@@ -1,40 +1,36 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class swea_9229 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner("""
-1
-4 100
-80 80 60 60
-				""");
-		
+		Scanner sc = new Scanner(System.in);
+
 		int T = sc.nextInt();
-		
+
 		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
 			int M = sc.nextInt();
 			int[] arr = new int[N];
-			
+
 			for (int i = 0; i < N; i++) {
 				arr[i] = sc.nextInt();
 			}
-			
+
+			Arrays.sort(arr);
 			int start = 0;
 			int end = N - 1;
-			
-			while(start < end && arr[start] + arr[end] != M) {
+			int answer = -1;
+
+			while (start < end) {
 				if (arr[start] + arr[end] > M) {
 					end--;
-				} else if (arr[start] + arr[end] < M) {
+				} else {
+					answer = Math.max(answer, arr[start] + arr[end]);
 					start++;
 				}
 			}
-			
-			if(arr[start] + arr[end] > M) {
-				System.out.printf("#%d %d%n", tc, -1);
-			} else {
-				System.out.printf("#%d %d%n", tc, arr[start] + arr[end]);
-			}
+
+			System.out.printf("#%d %d%n", tc, answer);
 		}
 	}
 }
