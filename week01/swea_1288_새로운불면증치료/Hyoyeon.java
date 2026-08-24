8136a28bdda615b1b23a98b6c29fab2afd2412ad
koryeon <gydus241@gmail.com>
2026-08-24 15:00:13 +0900

    week05 1209

--- week01/swea_1288_새로운불면증치료/Hyoyeon.java
+++ week01/swea_1288_새로운불면증치료/Hyoyeon.java
@@ -1,5 +1,4 @@
 
-
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
@@ -35,28 +34,23 @@ public class Hyoyeon {
 		for (int test_case = 1; test_case <= T; test_case++) {
 
 			int num = sc.nextInt();
-			int a= num;
+			int a = num;
 			int cnt = 0;
 			Set<Integer> set = new HashSet();
 			while (set.size() != 10) {
 				cnt += 1;
-				//a *= cnt;
-				num =a;
-				num*=cnt;
+				num = a;
+				num *= cnt;
 				while (num != 0) {
-					if(num<10) {
+					if (num < 10) {
 						set.add(num);
 					}
 					set.add(num % 10);
-					//System.out.println(set);
 					num /= 10;
 				}
 
-				
-
-				
 			}
-			System.out.println("#" + test_case + " " + a*cnt);
+			System.out.println("#" + test_case + " " + a * cnt);
 
 		}
 	}
